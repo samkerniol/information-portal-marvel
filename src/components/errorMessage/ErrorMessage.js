2d3d5b9ad9379ec0f8bad81707598c7bb19485e7
samkerniol <pavelkorol3v@gmail.com>
2021-10-20 02:55:00 +0300

    completed component RandomChar

---
+++ src/components/errorMessage/ErrorMessage.js
@@ -0,0 +1,9 @@
+import error from './error.gif';
+
+const ErrorMessage = () => {
+    return (
+        <img src={error} alt='error' style={{margin: '0 auto', display: 'block'}}/>
+    )
+}
+
+export default ErrorMessage;
---
+++ src/components/spinner/Spinner.js
@@ -0,0 +1,7 @@
+import spinner from './spinner.gif';
+
+const Spinner = () => {
+    return <img src={spinner} alt='spinner' style={{margin: '0 auto', display: 'block'}}/>
+}
+
+export default Spinner;
